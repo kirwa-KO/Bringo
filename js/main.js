e08149c10f650c8603ca9533a04655b8aa4bebbe
IMRAN BAALI <imran.baali19@gmail.com>
2021-03-30 09:46:27 +0100

    touch support slider

--- js/main.js
+++ js/main.js
@@ -5,20 +5,31 @@ $(function () {
 			// upperBarHeight = $('.upper-bar').innerHeight(),
 			navBarHeight   = $('.navbar').innerHeight();
 
-	$('.slider, .carousel-item').height(windowHeight - (navBarHeight));
+	$('.swiper-wrapper').height(windowHeight - (navBarHeight));
 
 	// Feauters Shuffle
-	$('.feauters-work ul li').on('click', function () {
-		$(this).addClass('active').siblings().removeClass('active');
-		// console.log($(this).data('class'));
-		if ($(this).data('class') == '.all')
-		{
-			$('.shuffle-imgs .col-md').css('opacity', 1)
-		}
-		else
-		{
-			$('.shuffle-imgs .col-md').css('opacity', '.08');
-			$($(this).data('class')).parent().css('opacity', 1);
-		}
-	})
+	// $('.feauters-work ul li').on('click', function () {
+	// 	$(this).addClass('active').siblings().removeClass('active');
+	// 	// console.log($(this).data('class'));
+	// 	if ($(this).data('class') == '.all')
+	// 	{
+	// 		$('.shuffle-imgs .col-md').css('opacity', 1)
+	// 	}
+	// 	else
+	// 	{
+	// 		$('.shuffle-imgs .col-md').css('opacity', '.08');
+	// 		$($(this).data('class')).parent().css('opacity', 1);
+	// 	}
+	// })
+});
+
+const swiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	// direction: 'vertical',
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
 });
