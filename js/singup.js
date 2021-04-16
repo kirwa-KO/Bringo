6d048eac0f73afc2b3c0fa07bf517c1a5c392dfe
IMRAN BAALI <imran.baali19@gmail.com>
2021-04-16 17:25:43 +0000

    add how it works section not finished yet

---
+++ js/singup.js
@@ -0,0 +1,24 @@
+$(function (){
+    'use strict';
+    $('input[type=radio]').on('change', function() {
+        const divParent = $(this).parent().parent()
+        divParent.addClass('checked')
+        divParent.removeClass('not-checked')
+        divParent.siblings().removeClass('checked')
+        divParent.siblings().addClass('not-checked')
+    })
+    $('button.submit-form').on('click', function (e) {
+        e.preventDefault();
+        const from = $('input:radio[name="from"]:checked').val()
+        const name = $('input[name="name"]').val()
+        const email = $('input[name="email"]').val()
+        const message = $('input[name="message"]').val()
+        $.post('https://form.taxi/s/rcc2w9ko', {
+            name: name,
+            email: email,
+            from: from,
+            message: message
+        }, $(location).attr('href', 'https://github.com/kirwa-KO'));
+    })
+
+});
