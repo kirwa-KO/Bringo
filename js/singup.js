2ecce86a37c29ed40d352eb0dcf4f2222a9e0c94
IMRAN BAALI <imran.baali19@gmail.com>
2021-04-17 09:32:46 +0000

    add congratulation page and input validation before sed

--- js/singup.js
+++ js/singup.js
@@ -1,24 +1,31 @@
-$(function (){
-    'use strict';
-    $('input[type=radio]').on('change', function() {
-        const divParent = $(this).parent().parent()
-        divParent.addClass('checked')
-        divParent.removeClass('not-checked')
-        divParent.siblings().removeClass('checked')
-        divParent.siblings().addClass('not-checked')
-    })
-    $('button.submit-form').on('click', function (e) {
-        e.preventDefault();
-        const from = $('input:radio[name="from"]:checked').val()
-        const name = $('input[name="name"]').val()
-        const email = $('input[name="email"]').val()
-        const message = $('input[name="message"]').val()
-        $.post('https://form.taxi/s/rcc2w9ko', {
-            name: name,
-            email: email,
-            from: from,
-            message: message
-        }, $(location).attr('href', 'https://github.com/kirwa-KO'));
-    })
+$(function() {
+	'use strict';
+	$('input[type=radio]').on('change', function() {
+		const divParent = $(this).parent().parent()
+		divParent.addClass('checked')
+		divParent.removeClass('not-checked')
+		divParent.siblings().removeClass('checked')
+		divParent.siblings().addClass('not-checked')
+	})
 
+  function validateEmail (email) {
+    const emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test(email);
+  }
+
+	$('#singUpForm').submit(function(e) {
+    e.preventDefault();
+		const from = $('input:radio[name="from"]:checked').val()
+		const name = $('input[name="name"]').val()
+		const email = $('input[name="email"]').val()
+    const message = $('input[name="message"]').val()
+		if (from !== "" && name !== "" && validateEmail(email) === true) {
+			$.post('https://form.taxi/s/rcc2w9ko', {
+				name: name,
+				email: email,
+				from: from,
+				message: message
+			}, $(location).attr('href', '/congratulation.html'));
+    }
+	})
 });
